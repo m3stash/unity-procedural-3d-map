66de515960efc53fb8299b32b2f1d055158d6357
laurent <laurent.duchaussoy59@gmail.com>
2021-03-18 00:10:52 +0100

    feat(LandMassGeneration): add local and global normalise
    fix issue with bag gap between 2 chunks

--- Assets/EndlessTerrain.cs
+++ Assets/EndlessTerrain.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EndlessTerrain : MonoBehaviour {
 
+    const float scale = 1f;
+
     const float viewerMoveThresholdForChunkUpdate = 25f;
     const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
 
@@ -20,7 +22,7 @@ public class EndlessTerrain : MonoBehaviour {
     int chunksVisibleInViewDst;
 
     Dictionary<Vector2, TerrainChunk> terrainChunkDictionnary = new Dictionary<Vector2, TerrainChunk>();
-    List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+    static List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
     private void Start() {
         mapGenerator = FindObjectOfType<MapGenerator>();
@@ -33,7 +35,7 @@ public class EndlessTerrain : MonoBehaviour {
     }
 
     private void Update() {
-        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z) / scale;
 
         if ((viewerPositionOld - viewerPosition).sqrMagnitude > sqrViewerMoveThresholdForChunkUpdate) {
             viewerPositionOld = viewerPosition;
@@ -57,9 +59,6 @@ public class EndlessTerrain : MonoBehaviour {
 
                 if (terrainChunkDictionnary.ContainsKey(viewedChunkCoord)) {
                     terrainChunkDictionnary[viewedChunkCoord].UpdateTerrainChunk();
-                    if (terrainChunkDictionnary[viewedChunkCoord].IsVisible()) {
-                        terrainChunksVisibleLastUpdate.Add(terrainChunkDictionnary[viewedChunkCoord]);
-                    }
                 } else {
                     terrainChunkDictionnary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, detailLevels, transform, mapMaterial));
                 }
@@ -94,7 +93,8 @@ public class EndlessTerrain : MonoBehaviour {
             meshFilter = meshObject.AddComponent<MeshFilter>();
             meshRenderer.material = material;
 
-            meshObject.transform.position = positionV3;
+            meshObject.transform.position = positionV3 * scale;
+            meshObject.transform.localScale = Vector3.one * scale;
             meshObject.transform.parent = parent;
             SetVisible(false);
 
@@ -142,6 +142,8 @@ public class EndlessTerrain : MonoBehaviour {
                         }
                     }
                 }
+
+                terrainChunksVisibleLastUpdate.Add(this);
 
                 SetVisible(visible);
             }
