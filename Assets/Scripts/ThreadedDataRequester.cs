670a931661c8088a30509a8549a2cb6d8ae71309
laurent <laurent.duchaussoy59@gmail.com>
2021-04-01 00:04:35 +0200

    feat(HDRP): change render pipeline from URL to URDP
    - refacto water shader
    - refacto terrain shader

--- Assets/Scripts/ThreadedDataRequester.cs
+++ Assets/Scripts/ThreadedDataRequester.cs
@@ -6,46 +6,46 @@ using System.Threading;
 
 public class ThreadedDataRequester : MonoBehaviour {
 
-    static ThreadedDataRequester instance;
-    Queue<ThreadInfo> dataQueue = new Queue<ThreadInfo>();
+	static ThreadedDataRequester instance;
+	Queue<ThreadInfo> dataQueue = new Queue<ThreadInfo>();
 
-    private void Awake() {
-        instance = FindObjectOfType<ThreadedDataRequester>();
-    }
+	void Awake() {
+		instance = FindObjectOfType<ThreadedDataRequester>();
+	}
 
-    public static void RequestData(Func<object> generateData, Action<object> callback) {
-        ThreadStart threadStart = delegate {
-            instance.DataThread(generateData, callback);
-        };
+	public static void RequestData(Func<object> generateData, Action<object> callback) {
+		ThreadStart threadStart = delegate {
+			instance.DataThread(generateData, callback);
+		};
 
-        new Thread(threadStart).Start();
-    }
+		new Thread(threadStart).Start();
+	}
+	// Func<object> represent delegate with a return type of object with no parameters
+	void DataThread(Func<object> generateData, Action<object> callback) {
+		object data = generateData();
+		lock (dataQueue) {
+			dataQueue.Enqueue(new ThreadInfo(callback, data));
+		}
+	}
 
-    // Func<object> represent delegate with a return type of object with no parameters
-    void DataThread(Func<object> generateData, Action<object> callback) {
-        object data = generateData();
-        lock (dataQueue) {
-            dataQueue.Enqueue(new ThreadInfo(callback, data));
-        }
-    }
 
-    void Update() {
-        if (dataQueue.Count > 0) {
-            for (int i = 0; i < dataQueue.Count; i++) {
-                ThreadInfo threadInfo = dataQueue.Dequeue();
-                threadInfo.callback(threadInfo.parameter);
-            }
-        }
-    }
+	void Update() {
+		if (dataQueue.Count > 0) {
+			for (int i = 0; i < dataQueue.Count; i++) {
+				ThreadInfo threadInfo = dataQueue.Dequeue();
+				threadInfo.callback(threadInfo.parameter);
+			}
+		}
+	}
 
-    struct ThreadInfo {
-        public readonly Action<object> callback;
-        public readonly object parameter;
+	struct ThreadInfo {
+		public readonly Action<object> callback;
+		public readonly object parameter;
 
-        public ThreadInfo(Action<object> callback, object parameter) {
-            this.callback = callback;
-            this.parameter = parameter;
-        }
-    }
+		public ThreadInfo(Action<object> callback, object parameter) {
+			this.callback = callback;
+			this.parameter = parameter;
+		}
 
+	}
 }
